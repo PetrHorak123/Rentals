b03631d4d91b38e6a9f70d7224cfa312f196bd12
honcyk <honcky99@gmail.com>
2019-01-14 19:20:47 +0100

    Connection string for migrations (it's implementing COntextFactory, to be used in other places, like possible other projects)

---
+++ Rentals.DL/EntitiesContextDbFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Rentals.DL
+{
+	public class EntitiesContextDbFactory : IDesignTimeDbContextFactory<EntitiesContext>
+	{
+		public EntitiesContext CreateDbContext(string[] args)
+		{
+			var builder = new DbContextOptionsBuilder<EntitiesContext>();
+			builder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Integrated Security=True;");
+			builder.UseLazyLoadingProxies();
+
+			return new EntitiesContext(builder.Options);
+		}
+	}
+}
